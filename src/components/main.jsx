a91dcbf8d732222225775cd495cd743f8b65756d
Olga Melnikova <omelnikova@accesssoftek.com>
2022-03-25 08:41:48 +0400

    add images from figma, add importAll into array of images

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
-import logo from "../img/Logo.svg";
-import env from "../img/GroupEnvironment.svg";
-import hes from "../img/GroupHealthSafety.svg";
-import qual from "../img/GroupQuality.svg";
-import listicon from "../img/Group.svg";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import { Link } from "react-router-dom";
+
+function importAll(r) {
+    return r.keys().map(r);
+}
+
+const images = importAll(require.context("../img", false, /\.(png|jpe?g|svg)$/));
 
 const Main = () => {
     const [show, setShow] = useState(false);
@@ -17,17 +18,17 @@ const Main = () => {
             <div className="px-4 py-5 text-center">
                 <img
                     className="d-block mx-auto mb-2 mt-2"
-                    src={logo}
+                    src={images[4].default}
                     alt="logo"
                     width="200"
-                    height="50"
+                    height="40"
                 />
                 <p className="display-6 colorText">Build the statistics. Improve yourself.</p>
                 <div className="col-lg-6 mx-auto">
                     <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
                         <img
                             className="d-block mx-auto mb-2 mt-2"
-                            src={qual}
+                            src={images[3].default}
                             alt="1"
                             width="280"
                             height="380"
@@ -35,8 +36,8 @@ const Main = () => {
 
                         <a href="#">
                             <img
-                                className="d-block mx-auto mb-2 mt-2"
-                                src={hes}
+                                className="d-block mx-auto mb-2 mt-2 "
+                                src={images[2].default}
                                 alt="2"
                                 width="280"
                                 height="380"
@@ -45,19 +46,73 @@ const Main = () => {
                         </a>
                         <img
                             className="d-block mx-auto mb-2 mt-2"
-                            src={env}
+                            src={images[1].default}
                             alt="3"
                             width="280"
                             height="380"
                         />
                     </div>
-                    <Link to="/list">
+                    <Link to="/list" className="p-2">
                         <img
-                            className="d-block mx-auto mb-2 mt-2"
-                            src={listicon}
+                            className="mx-auto mb-2 mt-2"
+                            src={images[0].default}
                             alt="3"
-                            width="50"
-                            height="50"
+                            width="40"
+                            height="40"
+                        />
+                    </Link>
+                    <Link to="/" className="p-2">
+                        <img
+                            className="mx-auto mb-2 mt-2"
+                            src={images[8].default}
+                            alt="3"
+                            width="40"
+                            height="40"
+                        />
+                    </Link>
+                    <Link to="/" className="p-2">
+                        <img
+                            className="mx-auto mb-2 mt-2"
+                            src={images[7].default}
+                            alt="3"
+                            width="40"
+                            height="40"
+                        />
+                    </Link>
+                    <Link to="/" className="p-2">
+                        <img
+                            className="mx-auto mb-2 mt-2"
+                            src={images[9].default}
+                            alt="3"
+                            width="40"
+                            height="40"
+                        />
+                    </Link>
+                    <Link to="/" className="p-2">
+                        <img
+                            className="mx-auto mb-2 mt-2"
+                            src={images[5].default}
+                            alt="3"
+                            width="40"
+                            height="40"
+                        />
+                    </Link>
+                    <Link to="/" className="p-2">
+                        <img
+                            className="mx-auto mb-2 mt-2"
+                            src={images[6].default}
+                            alt="3"
+                            width="40"
+                            height="40"
+                        />
+                    </Link>
+                    <Link to="/" className="p-2">
+                        <img
+                            className="mx-auto mb-2 mt-2"
+                            src={images[10].default}
+                            alt="3"
+                            width="40"
+                            height="40"
                         />
                     </Link>
                 </div>
