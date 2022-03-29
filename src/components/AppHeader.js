c67db9d524c90b78258b9938145c335037f7cadc
Olga Melnikova <omelnikova@accesssoftek.com>
2022-03-29 18:31:06 +0400

    made corrections to font style and icons position

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -2,18 +2,23 @@ import React from "react";
 import logo from "../img/Logo.svg";
 
 const AppHeader = () => {
-	return (
-		<div className="px-4 py-5 text-center">
-			<img
-				className="d-block mx-auto mb-2 mt-2"
-				src={logo}
-				alt="logo"
-				width="200"
-				height="50"
-			/>
-			<p className="display-6 colorText">Build the statistics. Improve yourself.</p>
-		</div>
-	);
+    return (
+        <div className="px-4 py-3 text-center">
+            <img
+                className="d-block mx-auto mb-2 mt-2"
+                src={logo}
+                alt="logo"
+                width="200"
+                height="50"
+            />
+            <p
+                className="display-6
+			 colorText weightText"
+            >
+                Build the statistics. Improve yourself.
+            </p>
+        </div>
+    );
 };
 
 export default AppHeader;
--- src/components/menuPanel/MenuPanel.js
+++ src/components/menuPanel/MenuPanel.js
@@ -1,91 +1,80 @@
 import React from "react";
 import { Link } from "react-router-dom";
-// import listicon from "../../img/Group.svg";
-import { People, Reports, Rows, Settings, Works, World, listicon } from '../../img/Icons/indexIcon'
-// function importAll(r) {
-// 	return r.keys().map(r);
-// }
-
-// const images = importAll(require.context("../img/icons", false, /\.(png|jpe?g|svg)$/));
+import { People, Reports, Rows, Settings, Works, World, listicon } from "../../img/Icons/indexIcon";
 
 const MenuPanel = () => {
+    return (
+        <div className="colorBg">
+            <div className=" text-center">
+                <div className="col-lg-6 mx-auto">
+                    <Link to="/list">
+                        <img
+                            className=" mx-auto mb-2 mt-2"
+                            src={listicon}
+                            alt="3"
+                            width="50"
+                            height="50"
+                        />
+                    </Link>
 
-	return (
-		<div className="colorBg">
-			<div className=" text-center">
-				<div className="col-lg-6 mx-auto">
-					<Link to="/list">
-						<img
-							className=" mx-auto mb-2 mt-2"
-							src={listicon}
-							alt="3"
-							width="50"
-							height="50"
-						/>
-					</Link>
-
-					<Link to="/" className="p-2">
-						<img
-							className="mx-auto mb-2 mt-2"
-							src={Settings}
-							alt="3"
-							width="40"
-							height="40"
-						/>
-					</Link>
-					<Link to="/" className="p-2">
-						<img
-							className="mx-auto mb-2 mt-2"
-							src={Rows}
-							alt="3"
-							width="40"
-							height="40"
-						/>
-					</Link>
-					<Link to="/" className="p-2">
-						<img
-							className="mx-auto mb-2 mt-2"
-							src={Works}
-							alt="3"
-							width="40"
-							height="40"
-						/>
-					</Link>
-					<Link to="/" className="p-2">
-						<img
-							className="mx-auto mb-2 mt-2"
-							src={People}
-							alt="3"
-							width="40"
-							height="40"
-						/>
-					</Link>
-					<Link to="/" className="p-2">
-						<img
-							className="mx-auto mb-2 mt-2"
-							src={Reports}
-							alt="3"
-							width="40"
-							height="40"
-						/>
-					</Link>
-					<Link to="/" className="p-2">
-						<img
-							className="mx-auto mb-2 mt-2"
-							src={World}
-							alt="3"
-							width="40"
-							height="40"
-						/>
-					</Link>
-
-
-
-				</div>
-
-			</div>
-		</div>
-	);
+                    <Link to="/" className="p-2">
+                        <img
+                            className="mx-auto mb-2 mt-2"
+                            src={Settings}
+                            alt="3"
+                            width="40"
+                            height="40"
+                        />
+                    </Link>
+                    <Link to="/" className="p-2">
+                        <img
+                            className="mx-auto mb-2 mt-2"
+                            src={Rows}
+                            alt="3"
+                            width="40"
+                            height="40"
+                        />
+                    </Link>
+                    <Link to="/" className="p-2">
+                        <img
+                            className="mx-auto mb-2 mt-2"
+                            src={Works}
+                            alt="3"
+                            width="40"
+                            height="40"
+                        />
+                    </Link>
+                    <Link to="/" className="p-2">
+                        <img
+                            className="mx-auto mb-2 mt-2"
+                            src={People}
+                            alt="3"
+                            width="40"
+                            height="40"
+                        />
+                    </Link>
+                    <Link to="/" className="p-2">
+                        <img
+                            className="mx-auto mb-2 mt-2"
+                            src={Reports}
+                            alt="3"
+                            width="40"
+                            height="40"
+                        />
+                    </Link>
+                    <Link to="/" className="p-2">
+                        <img
+                            className="mx-auto mb-2 mt-2"
+                            src={World}
+                            alt="3"
+                            width="40"
+                            height="40"
+                        />
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default MenuPanel;
