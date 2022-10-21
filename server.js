78798aeaf05f1cef6ed9e36a481afb781be5ee74
synikolaev <synikolaev@inno.tech>
2022-10-21 18:17:23 +0300

    wip

--- server.js
+++ server.js
@@ -5,18 +5,11 @@ var fs = require('fs')
 
 app.set('port', 3000)
 app.use(express.static(__dirname + '/packages/heqs-ui-app/dist'))
-app.engine('html', require('ejs').renderFile)
-app.set('view engine', 'ejs')
-app.set('views', __dirname + '/packages/heqs-ui-app/dist')
 
-app.get('/', function (request, response) {
-  response.render('index.html')
+app.get('*', function (request, response) {
+  response.sendFile(__dirname + '/packages/heqs-ui-app/dist/index.html')
 })
 
 app.listen(app.get('port'), function () {
-  if (process.env.DYNO) {
-    console.log('This is on Heroku..!!')
-    fs.openSync('/tmp/app-initialized', 'w')
-  }
   console.log('Node app is running on port', app.get('port'))
 })
