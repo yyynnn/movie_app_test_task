d2d614964c1b7887fc525bb4e59b52cd3605c0f2
synikolaev <synikolaev@inno.tech>
2022-11-01 18:58:25 +0300

    orval and generated types/hooks added

--- orval.config.js
+++ orval.config.js
@@ -2,6 +2,10 @@ module.exports = {
   'swagger-api': {
     // input: `./swagger.json`,
     input: `https://dev.heqsapp.com/docs/api-docs.json`,
-    output: `./packages/heqs-ui-app/src/features/api/generated/index.ts`
+    output: {
+      target: './packages/heqs-ui-app/src/features/api/generated/endpoints.ts',
+      schemas: './packages/heqs-ui-app/src/features/api/generated/models',
+      client: 'react-query'
+    }
   }
 }
---
+++ packages/heqs-ui-app/src/features/api/generated/models/index.ts
@@ -0,0 +1,4 @@
+export * from './ticket'
+export * from './ticketRootCausesItem'
+export * from './user'
+export * from './workcenter'
